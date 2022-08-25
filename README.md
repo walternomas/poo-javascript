1178bdd4cf7ebecb568cddc128ec37bd20efe827
walternomas <w.barriosvazquez@gmail.com>
2022-08-25 13:42:04 -0300

    update readme main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # poo-javascript
 Curso Básico de Programación Orientada a Objetos con JavaScript
+
+## Temario
+- Pilares de POO
+- Objetos a profundidad
+- SOLID
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Objeto literal
 const natalia = {
   name: "Natalia",
   age: 20,
@@ -64,4 +65,124 @@ const miguelito = new Student2({
     "Curso de Principios de visualización de Datos para BI"
   ]
 });
+
+// Ventajas de la POO en JS
+
+//OBJETOS LITERALES
+const Juan1 = {
+  name: "JuanDC",
+  username: "juandc",
+  points: 100,
+  socialMedia: {
+    twitter: "fjuandc",
+    instagram: "fjuandc",
+    facebook: undefined,
+  },
+  approvedCourses: [
+    "Curso Definitivo de HTML y CSS",
+    "Curso Práctico de HTML y CSS",
+  ],
+  learningPaths: [
+    {
+      name: "Escuela de Desarrollo Web",
+      courses: [
+        "Curso definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+        "Curso de Responsive Design",
+        "Curso de Sistemas de Diseño"
+      ],
+    },
+    {
+      name: "Escuela de Videojuegos",
+      courses: [
+        "Curso Introducción a la producción de videojuegos",
+        "Curso de Unreal Engine",
+        "Curso de Unity 3D",
+      ],
+    },
+  ],
+};
+
+const miguelito1 = {
+  name: "Miguelito",
+  username: "miguelitofeliz",
+  points: 1000,
+  socialMedia: {
+    twitter: "miguelitofeliz",
+    instagram: "miguelito_feliz",
+    facebook: undefined,
+  },
+  approvedCourses: [
+    "Curso DataBusiness",
+    "Curso DataViz",
+  ],
+  learningPaths: [
+    {
+      name: "Escuela de Desarrollo Web",
+      courses: [
+        "Curso definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+        "Curso de Responsive Design",
+      ],
+    },
+    {
+      name: "Escuela de Data Science",
+      courses: [
+        "Curso DataBusiness",
+        "Curso DataViz",
+        "Curso Tableau",
+      ],
+    },
+  ],
+};
+
+//POO Transformamos los anteriores Objetos en Clases para luego instanciarlos.
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+const juan2 = new Student({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juanito@gmail.com",
+  twitter: "fjuandc",
+});
+
+const miguelito2 = new Student({
+  name: "Miguelito",
+  username: "miguelitofeliz",
+  email: "miguelito@gmail.com",
+  instagram: "miguelito_feliz",
+});
+
+//Abstracción
+class LearningPaths {
+  constructor({
+    name,
+    courses = [],
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
 
